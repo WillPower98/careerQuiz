21cb28f72ebf9bd09ef8c184ff378f11ca4cc9f4
Will M <munozwi@bu.edu>
2020-07-24 17:52:05 -0400

    added html functionality

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
