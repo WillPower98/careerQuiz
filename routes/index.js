cede492025e112e97805fac64639707866a765b6
Will M <munozwi@bu.edu>
2020-08-04 16:09:21 -0400

    elimination of careers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var career_points = {};
 
 const q0 = {
   title: "What is your preferred work location?",
@@ -15,15 +16,53 @@ const q1 = {
 };
 
 var questions = [q0, q1]
-var counter = 0
+var qcounter = 0
 
 function nextQuestion(){
-  counter++;
-  return questions[counter]
+  qcounter++;
+  return questions[qcounter]
 }
 
 function thisQuestion(){
-  return questions[counter]
+  return questions[qcounter]
+}
+function processQuestion(userAnswer){
+  if(qcounter == 0){
+    filterCareers(userAnswer)
+  }
+}
+
+function filterCareers(pref_location){
+  let fso = ["Consular Officers", "Economic Officers", "Management Officers", "Political Officers", "Public Diplomacy Officers"]
+      let fss = ["Medical and Health", "Information Technology", "Engineering", "International Programs and English Language", "Law Enforcement and Security"]
+      let cs = ["Foreign Affairs Officer", "Information Technology Management​", "Intelligence Series", "Public Affairs", "Language Specialist"]
+
+      
+      //assigning career tracks based off of pref location
+      let tracks = []
+      switch(pref_location){
+              case("U.S."):
+                      tracks = cs.concat(fss)
+                      break;
+              case("International"):
+                      tracks = fso.concat(fss)
+                      break;
+              case("Both"):
+                      tracks = fso.concat(fss).concat(cs)
+                      break;
+              default:
+      }
+
+      console.log("Possible tracks are " + tracks.toString())
+
+      let career_table = {}
+      //creating the career tracks dict
+      for(let i = 0; i < tracks.length; i++){
+              career_table[tracks[i]] = 0
+      }
+
+      career_points = career_table
+      console.log(career_points)
 }
 
 /* The client with first access the get method. The post method will be used
@@ -39,10 +78,11 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
 
-    const preferredLocation = req.body.userChoice;
+    const userAnswer = req.body.userChoice;
     const questionTitle = req.body.questionTitle
     console.log("The title of the question was: " + questionTitle);
-    console.log("The answer of the user was: " + preferredLocation);
+    console.log("The answer of the user was: " + userAnswer);
+    processQuestion(userAnswer, )
     res.render('index', { title: 'career quiz', question: nextQuestion()});
 });
 
