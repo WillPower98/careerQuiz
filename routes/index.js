c6c6fb40ed7d179f0b0fafc893b3c158455de050
Will M <munozwi@bu.edu>
2020-08-04 16:00:10 -0400

    iterated over a list of questions

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 
-const Question = {
+const q0 = {
   title: "What is your preferred work location?",
   image: "res/img_nature_wide.jpg",
   choices: ["U.S.", "International", "Both"]
 };
 
-const NextQuestion = {
+const q1 = {
   title: "Are you a US Citizen?",
   image: "res/img_snow_wide.jpg",
   choices: ["Yes", "No", "That's classified!"]
 };
 
+var questions = [q0, q1]
+var counter = 0
 
+function nextQuestion(){
+  counter++;
+  return questions[counter]
+}
+
+function thisQuestion(){
+  return questions[counter]
+}
 
 /* The client with first access the get method. The post method will be used
 for subsequent interactions with the client. The post method checks the question
@@ -24,7 +34,7 @@ currently, only two question objects are defined, but we can add more very
 easily. */
 
 router.get("/", (req, res) => {
-  res.render('index', { title: 'career quiz', question: Question});
+  res.render('index', { title: 'career quiz', question: thisQuestion()});
 });
 
 router.post("/", (req, res) => {
@@ -33,7 +43,7 @@ router.post("/", (req, res) => {
     const questionTitle = req.body.questionTitle
     console.log("The title of the question was: " + questionTitle);
     console.log("The answer of the user was: " + preferredLocation);
-    res.render('index', { title: 'career quiz', question: NextQuestion});
+    res.render('index', { title: 'career quiz', question: nextQuestion()});
 });
 
 module.exports = router;
