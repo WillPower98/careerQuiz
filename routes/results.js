e78f81b66e307f47cca9a037763199738a2cff1b
Will M <munozwi@bu.edu>
2020-08-18 18:24:18 -0400

    created form and tried parsing

--- routes/results.js
+++ routes/results.js
@@ -2,21 +2,30 @@ const express = require('express');
 const router = express.Router();
 let quizSessions = require('../modules/quizSessionHandler').quizSessions;
 
+
 router.get("/results/sessionId/:sessionId", (req, res) => {
     const quizSessionId = req.params.sessionId;
     console.log(`Checking that endpoint has access to global map and that
                   id is present... ${quizSessions.has(quizSessionId)}`);
     console.log(quizSessionId);
-    res.render('preResults', {
+
+    res.render('results', {
         answers: quizSessionId
     })
+
+   
+
 });
 
 
-router.post('/results', function(req, res, next) {
-
-    // Http body req.body.blahblahblah
+router.post('/results/sessionId/:sessionId', function(req, res, next) {
+    
     console.log(QuizSession.getCareerRecommendations());
+    // const email = req.body.email;
+    // const firstName = req.body.firstName;
+    // const lastName = req.body.lastName
+    console.log(JSON.parse(req))
+
     res.render(
         'results', {careerRecs: QuizSession.getCareerRecommendations()}
     )
