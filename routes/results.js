ae6fbc881153a9a7e54e306f314f5cfac14bd5f7
Manuela Rodriguez <manurod19@gmail.com>
2020-08-18 23:54:59 -0400

    call updateCareerMapScores function

--- routes/results.js
+++ routes/results.js
@@ -3,31 +3,6 @@ const router = express.Router();
 let quizSessions = require('../modules/quizSessionHandler').quizSessions;
 
 
-/* The career map is updated at the end of every quiz session by
-    popping all of the elements in the answerStack */
-// Choice object: {abbreviation: property}
-
-function updateCareerMap(ActiveQuizSession) {
-
-  let careerMap = ActiveQuizSession.getCareerMap();
-
-  while (!ActiveQuizSession.emptyAnswerStack()) {
-
-    let userChoice = ActiveQuizSession.getLastUserAnswer();
-
-     for (let i = 0; i < userChoice.length; i++) {
-       let currentUserChoice = userChoice[i];
-       Object.keys(currentUserChoice).forEach(function(key, index) {
-         careerMap.get(key).score = careerMap.get(key).score + currentUserChoice[key];
-       });
-     }
-
-    ActiveQuizSession.setCareerRankingMap(careerMap);
-  };
-
-}
-
-
 router.get("/results/quizSessionId/:quizSessionId", (req, res) => {
     const quizSessionId = req.params.quizSessionId;
     console.log(`Checking that endpoint has access to global map and that
@@ -45,11 +20,15 @@ router.get("/results/quizSessionId/:quizSessionId", (req, res) => {
 
 router.post('/results/quizSessionId/:quizSessionId', function(req, res, next) {
 
+    const quizSessionId = req.params.quizSessionId;
+    const ActiveQuizSession = quizSessions.get(quizSessionId);
+    console.log(ActiveQuizSession.updateCareerMapScores());
+    
     // console.log(quizSessions.getCareerRecommendations());
     const email = req.body.email;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
-    const quizSessionId = req.params.quizSessionId;
+    //const quizSessionId = req.params.quizSessionId;
     console.log(email, firstName, lastName, quizSessionId)
 
     res.render(
